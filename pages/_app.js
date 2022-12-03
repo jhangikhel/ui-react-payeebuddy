5326dd822bc8f927ee27d737619cb651dedd9ce1
jhangikhel <gourav.jhangihel@gmail.com>
2022-12-03 10:37:01 +0530

    Work on Theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,22 @@
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
-  export default MyApp;
+import React from 'react';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+
+import createEmotionCache from '../utility/createEmotionCache';
+import lightTheme from '../styles/theme/lightTheme';
+import '../styles/globals.css';
+
+const clientSideEmotionCache = createEmotionCache();
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+export default MyApp;
